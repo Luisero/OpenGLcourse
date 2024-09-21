ab682da3b3bf858dc7aca31d7103cac15f3d4257
Luis Henrique <luismoura2005@gmail.com>
2024-09-21 17:03:56 -0300

    Zelda: indices

--- aula3/src/main.cpp
+++ aula3/src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <math.h>
 #include "glad.h"
 #include <GLFW/glfw3.h>
 
 using std::cout;
 using std::endl;
+
 
 
 const char *vertexShaderSource="#version 330 core\n"
@@ -29,7 +31,7 @@ int main()
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
-
+    
     glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
 
 
@@ -73,18 +75,27 @@ int main()
 
     GLfloat vertices[] =
 	{
-		-0.5f, -0.5f, 0.0f, // Lower left corner
-		0.5f, -0.5f , 0.0f, // Lower right corner
-		0.0f, 0.5f , 0.0f // Upper corner
+		-0.5f, -0.5f*float(sqrt(3))/3, 0.0f, // Lower left corner
+		0.5f, -0.5f*float(sqrt(3))/3 , 0.0f, // Lower right corner
+		0.0f, 0.5f*float(sqrt(3))*2/3 , 0.0f, // Upper corner
+        -0.5f/2,0.5f*float(sqrt(3))/6,0.0f,
+        0.5f/2,0.5f*float(sqrt(3))/6,0.0f,
+        0.0f, -0.5f*float(sqrt(3))/3,0.0f
 	};
 
+    GLuint indices[] = {
+        0,3,5,
+        3,2,4,
+        5,4,1
+    };
 
-    GLuint VAO, VBO;
+
+    GLuint VAO, VBO, EBO;
     glGenVertexArrays(1,&VAO);
     glBindVertexArray(VAO);
     
     glGenBuffers(1,&VBO);
-
+    glGenBuffers(1,&EBO);
     //VAO the current vertex buffer to bind
 
     //Bind VBO specifying its a array buffer
@@ -92,6 +103,8 @@ int main()
     //Verticies in the array buffer
     glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices, GL_STATIC_DRAW);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices),indices,GL_STATIC_DRAW);
     //COnffigure the vertex attributes and opengl will know how to read VBO
     glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE, 3*sizeof(float), (void*)0);
     //Enable the vertex atriibute
@@ -99,6 +112,7 @@ int main()
 
     glBindBuffer(GL_ARRAY_BUFFER,0);
     glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 
 
@@ -115,8 +129,8 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES,0,3);
-
+        //glDrawArrays(GL_TRIANGLES,0,3);
+        glDrawElements(GL_TRIANGLES,9,GL_UNSIGNED_INT,0);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
